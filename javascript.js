530aea40c236515906157bbb9b0011ec8364d6d3
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-05-10 13:47:50 -0400

    Add basic code structure for creating a grid with a hover effect that will fill in the squares.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
 const container = document.querySelector(".grid__container");
-const square = document.createElement("div");
-
-square.classList.add("grid__square");
 
 for (let i = 0; i < 16; i++) {
   const row = document.createElement("div");
@@ -10,7 +7,17 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const square = document.createElement("div");
     square.classList.add("grid__square");
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.background = "#424549";
+    });
     row.appendChild(square);
   }
   container.appendChild(row);
 }
+
+/* 
+separate these into functions at some point, with user input to be the paramater and the grid is created
+1. user input
+2. clear grid
+3. create grid
+*/
