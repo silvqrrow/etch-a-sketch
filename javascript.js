b98bf294fff9bc4a2a42518f1e8fb7f3f00df2e5
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-05-10 13:33:43 -0400

    Set up elements for the container, row, and squares for the board, as well as a way to produce them using a for loop

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const container = document.querySelector(".grid__container");
+const square = document.createElement("div");
+
+square.classList.add("grid__square");
+
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement("div");
+  row.classList.add("grid__row");
+
+  for (let j = 0; j < 16; j++) {
+    const square = document.createElement("div");
+    square.classList.add("grid__square");
+    row.appendChild(square);
+  }
+  container.appendChild(row);
+}
