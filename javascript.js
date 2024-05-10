e2c002eb72e9d0afe9bedec763d0813c883fb06f
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-05-10 16:38:37 -0400

    Refractor javascript file for readability and implement functionality of the grid size button

--- javascript.js
+++ javascript.js
@@ -1,23 +1,44 @@
 const container = document.querySelector(".grid__container");
+const btn = document.querySelector(".btn");
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.classList.add("grid__row");
+btn.onclick = () => {
+  let gridSize;
+  do {
+    gridSize = prompt("Input a grid size up to 100:", 16);
+  } while (gridSize > 100);
+  deleteGrid();
+  createGrid(gridSize);
+};
 
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("grid__square");
-    square.addEventListener("mouseover", (e) => {
-      e.target.style.background = "#424549";
-    });
-    row.appendChild(square);
+const createGrid = (gridSize) => {
+  // Create rows until the desired size
+  for (let rows = 0; rows < gridSize; rows++) {
+    const row = document.createElement("div");
+    row.classList.add("grid__row");
+
+    // Fill in each row with the squares
+    for (let squares = 0; squares < gridSize; squares++) {
+      const square = document.createElement("div");
+      square.classList.add("grid__square");
+      // Add a colour to the square if it is hovered
+      square.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = randomRGB();
+      });
+      row.appendChild(square);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
-}
+};
 
-/* 
-separate these into functions at some point, with user input to be the paramater and the grid is created
-1. user input
-2. clear grid
-3. create grid
-*/
+const deleteGrid = () => {
+  container.replaceChildren();
+};
+
+const randomRGB = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+// Default Grid
+createGrid(16);
